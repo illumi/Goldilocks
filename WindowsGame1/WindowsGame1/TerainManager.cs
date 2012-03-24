8adfffc957ac3821e193a8d1fc6a5623658c484f
thedamoes <nialloccart@gmail.com>
2012-03-24 00:12:25 +0000

    creating level

--- WindowsGame1/WindowsGame1/TerainManager.cs
+++ WindowsGame1/WindowsGame1/TerainManager.cs
@@ -13,60 +13,31 @@ namespace CGProj
     {
         public List<platForm> terrains = new List<platForm>();
         MouseState mPreviousMouseState;
+        public int currscreen;
+
+
 
         public TerainManager()
         {
             // initalise the map terains here
+            //1024
             
-            terrains.Add(new platForm(600, 380, 1, true, "platform", 0, 0, 200, 30));
-            terrains.Add(new platForm(200, 380, 1, true, "platform", 0, 0, 200, 30));
-            terrains.Add(new platForm(0, 570, 1, false, "level1", 0, 330, 245, 20));
-            terrains.Add(new platForm(243, 538, 1, false, "level1", 243, 298, 56, 100));//243 298			56 100
-            terrains.Add(new platForm(298, 602, 1, false, "level1", 298, 362, 341, 88));//298 362			341 88
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//503 282			76 20 
-            terrains.Add(new platForm(703, 602, 1, false, "level1", 703, 362, 616, 80)); // 703 362			616 80	
+            //terrains.Add(new platForm(600, 380, 1, true, "platform", 0, 0, 200, 30));
+            //terrains.Add(new platForm(200, 380, 1, true, "platform", 0, 0, 200, 30));
 
-            terrains.Add(new platForm(843, 555, 1, false, "level1", 843, 315, 96, 63));// 843 315			96 63
+            //screen 1
+            if (currscreen == 0)
+            {
+                terrains.Add(new platForm(0, 570, 1, false, "level1", 0, 330, 245, 20));
+                terrains.Add(new platForm(243, 538, 1, false, "level1", 243, 298, 56, 100));//243 298			56 100
+                terrains.Add(new platForm(298, 602, 1, false, "level1", 298, 362, 341, 88));//298 362			341 88
+                terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//503 282			76 20 
+                terrains.Add(new platForm(703, 602, 1, false, "level1", 703, 362, 616, 80)); // 703 362			616 80	
+                terrains.Add(new platForm(843, 555, 1, false, "level1", 843, 315, 96, 63));// 843 315			96 63
+                terrains.Add(new platForm(904, 506, 1, false, "level1", 904, 266, 96, 111));// 904 266			96 111
+                terrains.Add(new platForm(998, 538, 1, false, "level1", 998, 298, 21, 82));//998 298			21 82
+            }
 
-            terrains.Add(new platForm(904, 506, 1, false, "level1", 904, 266, 96, 111));// 904 266			96 111
-
-            terrains.Add(new platForm(998, 538, 1, false, "level1", 998, 298, 21, 82));//998 298			21 82
-
-            terrains.Add(new platForm(1414, 502, 1, false, "level1", 1414, 362, 96, 49));//1414 362		96 49
-
-           /* terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//1502 346		20 15
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//1443 283		97 20
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//1601 283		20 15
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//1518 378		247 63
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//1762 342		65 50
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//1823 315		136 79
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//1958 362		81 79
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//1697 205		20 15
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//1807 134		20 15
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//1902 90 		60 15
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//2104 363		55 80
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//2223 363		248 40
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//2463 331		150 50
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//2603 283		141 56
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//2743 235		76 74
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//2783 187		97 20
-
-            terrains.Add(new platForm(503, 522, 1, false, "level1", 503, 282, 76, 20));//2814 282		362 161 */
 
         }
 	
@@ -93,8 +64,7 @@ namespace CGProj
         {
             MouseState aCurrentMouseState = Mouse.GetState();
             UpdateMovingBlock(aCurrentMouseState);
-            mPreviousMouseState = aCurrentMouseState;
-
+           // mPreviousMouseState = aCurrentMouseState;
         }
 
 
@@ -109,11 +79,89 @@ namespace CGProj
                     {
                         if (p.movable == true)
                         {
+                            mPreviousMouseState = aCurrentMouseState;
                             p.CenterPointX = aCurrentMouseState.X;
                             p.CenterPointY = aCurrentMouseState.Y;
                         }
                     }
                 }
+
+            }
+
+
+        }
+
+
+        public void loadLevel(Engine.Pyhsics physicsEngine)
+        {
+            //screen 2
+            if (currscreen == 1)
+            {
+                physicsEngine.deRegisterFixedSolids();
+                terrains.Clear();
+
+                terrains.Add(new platForm(0, 602, 1, false, "level1", 1024, 362, 296, 15));
+
+                terrains.Add(new platForm(390, 602, 1, false, "level1", 1414, 362, 96, 49));//1414 362		96 49
+
+                terrains.Add(new platForm(478, 586, 1, false, "level1", 1502, 346, 20, 15));//1502 346		20 15
+
+                terrains.Add(new platForm(419, 523, 1, false, "level1", 1443, 283, 97, 20));//1443 283		97 20
+
+                terrains.Add(new platForm(577, 523, 1, false, "level1", 1601, 283, 20, 15));//1601 283		20 15
+
+                terrains.Add(new platForm(494, 618, 1, false, "level1", 1518, 378, 247, 63));//1518 378		247 63
+
+                terrains.Add(new platForm(738, 582, 1, false, "level1", 1762, 342, 65, 50));//1762 342		65 50
+
+                terrains.Add(new platForm(799, 555, 1, false, "level1", 1823, 315, 136, 79));//1823 315		136 79
+
+                terrains.Add(new platForm(934, 602, 1, false, "level1", 1958, 362, 81, 79));//1958 362		81 79
+
+                terrains.Add(new platForm(673, 445, 1, false, "level1", 1697, 205, 20, 15));//1697 205		20 15
+
+                terrains.Add(new platForm(783, 374, 1, false, "level1", 1807, 134, 20, 15));//1807 134		20 15
+
+                terrains.Add(new platForm(878, 330, 1, false, "level1", 1902, 90, 60, 15));//1902 90 		60 15
+            }
+
+
+            if (currscreen == 2)
+            {
+                physicsEngine.deRegisterFixedSolids();
+                terrains.Clear();
+
+                terrains.Add(new platForm(56, 603, 1, false, "level1", 2104, 363, 55, 80));//2104 363		55 80
+
+                terrains.Add(new platForm(175, 603, 1, false, "level1", 2223, 363, 248, 40));//2223 363		248 40
+
+                terrains.Add(new platForm(415, 571, 1, false, "level1", 2463, 331, 150, 50));//2463 331		150 50
+
+                terrains.Add(new platForm(555, 523, 1, false, "level1", 2603, 283, 141, 56));//2603 283		141 56
+
+                terrains.Add(new platForm(695, 475, 1, false, "level1", 2743, 235, 76, 74));//2743 235		76 74
+
+                terrains.Add(new platForm(735, 427, 1, false, "level1", 2783, 187, 97, 20));//2783 187		97 20
+
+                terrains.Add(new platForm(766, 522, 1, false, "level1", 2814, 282, 362, 161));//2814 282		362 161 
+            }
+
+            if (currscreen == 3)
+            {
+                physicsEngine.deRegisterFixedSolids();
+                terrains.Clear();
+
+
+                terrains.Add(new platForm(0, 522, (0.2f), false, "level1", 2814, 282, 362, 161));
+               // terrains.Add(new platForm(0, 603, 1, false, "platform", 0, 0, 200, 30));
+                terrains.Add(new platForm(800, 603, 1, false, "platform", 0, 0, 200, 30));
+
+                terrains.Add(new platForm(100, 200, 1, true, "Make", 0, 0, 160, 100));
+                terrains.Add(new platForm(260, 200, 1, true, "A", 0, 0, 160, 100));
+                terrains.Add(new platForm(420, 200, 1, true, "Bridge", 0, 0, 160, 100));
+                terrains.Add(new platForm(580, 200, 1, true, "With", 0, 0, 160, 100));
+                terrains.Add(new platForm(740, 200, 1, true, "Words", 0, 0, 160, 100));
+                terrains.Add(new platForm(200, 50, 1, false, "Instructions", 0, 0, 600, 150));
 
             }
 
